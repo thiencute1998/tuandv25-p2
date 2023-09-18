43ac3d1beeb4599760e2574bffdaf1c6f9a6b1cf
qhlinh <qhlinh86@gmail.com>
2023-09-18 21:03:20 +0700

    18/09

--- resources/views/viewer/layouts/aside.blade.php
+++ resources/views/viewer/layouts/aside.blade.php
@@ -3,7 +3,7 @@
     <div class="search-mobile">
         <form method="get" id="searchform-mobile" action="https://giaophanbacninh.org/">
             <button class="search-button" type="submit" value="Search"><i class="fa fa-search"></i></button>
-            <input type="text" id="s-mobile" name="s" title="Search" value="Search" onfocus="if (this.value == 'Search') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Search';}"  />
+            <input type="text" id="s-mobile" name="search" title="Tìm kiếm" value="Tìm kiếm" onfocus="if (this.value == 'Tìm kiếm') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Tìm kiếm';}"  />
         </form>
     </div><!-- .search-mobile /-->
 
