df194c5bc166abf3a9600b368bb838d4e8d63e38
qhlinh <qhlinh86@gmail.com>
2023-10-24 01:19:54 +0700

    2/10

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -92,7 +92,7 @@ class AppServiceProvider extends ServiceProvider
             //slide Home
             //$slideHome = Post::where('status', 1)->whereRaw('post_date <= "'.date('Y-m-d H:i:s').'"')->orderBy('post_date', 'desc')->take(10)->get();
             //$slideHome = Post::where('status', 1)->orderBy('created_at', 'desc')->take(10)->get();
-            $slideHome = Post::where('status', 1)->where('DATE(post_date)', '<=', "'".date('Y-m-d')."'")->orderBy('post_date', 'desc')->limit(10)->get();
+            $slideHome = Post::where('status', 1)->where('DATE(post_date)', '<=', "'".date('Y-m-d')."'")->orderBy('DATE(post_date)', 'desc')->limit(10)->get();
             // Lien he
             $contactWebsite = About::first();
             //Config
